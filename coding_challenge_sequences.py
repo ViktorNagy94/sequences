e7edae846d2f34685b24a9617785baefb6e51730
Nagy Viktor <nagy.viktor077@gmail.com>
2021-02-18 11:44:15 +0100

    Coding exercise done.

---
+++ coding_challenge_sequences.py
@@ -0,0 +1,37 @@
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac - Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flowers = []
+shrubs = []
+
+# write your code here
+for plant in data:
+    if "Shrub" in plant:
+        shrubs.append(plant)
+    elif "Flower" in plant:
+        flowers.append(plant)
+
+new_shrubs = [plant.replace(" - Shrub", "") for plant in shrubs]
+new_flowers = [plant.replace(" - Flower", "") for plant in flowers]
+
+print(new_shrubs)
+print(new_flowers)
